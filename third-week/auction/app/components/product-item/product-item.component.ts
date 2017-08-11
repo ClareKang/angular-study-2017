737b641164b3bfe21b9008c8bce1d86ffc99ce7e
ClareKang <shiningkey89@gmail.com>
2017-08-11 22:01:27 +0900

    add angular router auction example

--- third-week/auction/app/components/product-item/product-item.component.ts
+++ third-week/auction/app/components/product-item/product-item.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input } from '@angular/core';
-import StarsComponent from 'app/components/stars/stars.component';
 import { Product } from 'app/services/product.service';
 
 @Component({
   selector: 'auction-product-item',
+  styleUrls: ['app/components/product-item/product-item.css'],
   templateUrl: 'app/components/product-item/product-item.component.html'
 })
 export default class ProductItemComponent {
